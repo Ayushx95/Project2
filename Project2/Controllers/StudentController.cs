901848140e85ca5b52b5d81edce78bc448af357f
Ayush <ayushx95@gmail.com>
2026-09-24 20:11:57 +0530

    New Changes

--- Project2/Controllers/StudentController.cs
+++ Project2/Controllers/StudentController.cs
@@ -25,5 +25,61 @@ namespace Project2.Controllers
             if (student == null) return NotFound();
             return View(student);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Student student)
+        {
+            bool emailExists = _context.Students
+                .Any(s => s.Email == student.Email && s.Id != student.Id);
+
+            if (emailExists)
+            {
+                ModelState.AddModelError(
+                    "Email",
+                    "This Email is already Registered"
+                );
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(student);
+            }
+            if (student.Id == 0)
+            {
+                _context.Students.Add(student);
+            }
+            else
+            {
+                _context.Students.Update(student);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id, bool confirm = false)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+                return NotFound();
+
+            if (confirm)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+        public IActionResult Details(int id)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
     }
 }
+
--- Project2/Models/Student/Student.cs
+++ Project2/Models/Student/Student.cs
@@ -1,11 +1,21 @@
-﻿namespace Project2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Models
 {
     public class Student
     {
-        public int Id { get; set; } 
-        public string Name { get; set; }
-        public string Address { get; set; }
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name is Required")]
+        [StringLength(20,MinimumLength =3,ErrorMessage ="Name should be between 3 to 20")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="Address is Required")]
+        [StringLength(50,MinimumLength =5,ErrorMessage ="Student Adress mus be between 5 to 50")]
+        public string? Address { get; set; }
+        [Required(ErrorMessage = "Age is Required")]
+        [Range(5,20,ErrorMessage ="Age must be between 5 to 20")]
         public int Age { get; set; }
-        public string Email { get; set; }
+        [Required(ErrorMessage ="Email Address is Required")]
+        [EmailAddress(ErrorMessage ="Please Enter a Valid Email Address")]
+        public string? Email { get; set; }
     }
 }
