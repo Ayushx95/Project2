7e86efcef12bdabf7c582e2f69e0561f5ca63e40
Ayush <ayushx95@gmail.com>
2026-09-24 14:25:07 +0530

    Add project files.

---
+++ Project2/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project2.Data;
+using Project2.Models;
+
+namespace Project2.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var studentList = _context.Students.ToList();
+            return View(studentList);
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Student student = new Student();
+            if (id == null) return View(student);
+
+            student = _context.Students.Find(id.GetValueOrDefault());
+            if (student == null) return NotFound();
+            return View(student);
+        }
+    }
+}
---
+++ Project2/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project2.Models;
+
+
+namespace Project2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
